df8d208934289b355d40cea849e8ae0ec4707021
david waza <idemetoemediong2022@gmail.com>
2024-04-03 15:56:18 +0100

    fixed summit section mobile screens

---
+++ src/app/Components/SummitSection/MobileSummit.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+
+const MobileSummit = () => {
+  return (
+    <div className=" bg-summit py-20 lg:px-20 px-10">
+      <div>
+        <h1 className=" text-2xl lg:text-[40px] font-bold py-5 lg:leading-[60px]">
+          Why you should <br /> attend NDVP-SME <br /> Summit
+        </h1>
+      </div>
+      <div className=" border-[#F2F2F2] border-opacity-50 py-10">
+        <h1 className="font-bold text-5xl text-[#49FF91]">1</h1>
+        <p className="py-5 ">
+          NDVP-SME will aid the implementation and adoption of basic financial
+          literacy and digital literacy as the bedrock to youth & women
+          empowerment in the Niger Delta
+        </p>
+      </div>
+      <div className="py-5 w-full ">
+        <h1 className="font-bold text-5xl text-[#49FF91]">2</h1>
+        <p className="py-5">
+          ⁠NDVP-SME will aid the implementation ⁠ ⁠⁠Exposure of youth and women
+          businesses to potential investors and the exhibition of successful
+          youth and women entrepreneur businesses in the Niger Delta.
+        </p>
+      </div>
+      <div className="pb-5 w-full ">
+        <h1 className="font-bold text-5xl text-[#49FF91]">3</h1>
+        <p className="py-5">
+          Business fundamental literacy, apprenticeship and mentorship success
+          frameworks will be discussed, analysed and agreed on to promoting
+          youth & women entrepreneurship.
+        </p>
+      </div>
+      <div className="pb-5 w-full ">
+        <h1 className="font-bold text-5xl text-[#49FF91]">4</h1>
+        <p className="py-5">
+          The creation of high value-add jobs and development of sme as a base
+          for growing leadership talent among youths & women in Niger Delta
+        </p>
+      </div>
+      <div className="pb-5 w-full ">
+        <h1 className="font-bold text-5xl text-[#49FF91]">5</h1>
+        <p className="py-5">
+          ⁠Award recognition to leaders and humanitarians who have impacted
+          Niger Delta SME’s, youths & wome
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default MobileSummit;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,49 +3,140 @@ import VideoComponent from "./Components/VideoComponent/VideoComponent";
 import Summit from "./Components/SummitSection/Summit";
 import Keynote from "./Components/Keynotes/Keynote";
 import RegSection from "./Components/RegisterSection/RegSection";
+import Link from "next/link";
+import Image from "next/image";
+import MobileSummit from "./Components/SummitSection/MobileSummit";
 
 export default function Home() {
   return (
     <main>
       <Herobanner />
       <div className="bg-white py-20 lg:px-20 px-10">
-        <p className="text-black font-bold text-[20px] lg:text-[40px] text-center">
-          Welcome to Niger Delta Viewoint.
+        <p className="text-black font-bold text-[40px] text-center 2xl:block hidden">
+          Welcome to Niger Delta Viewpoint.
         </p>
-        <p className="text-black font-medium text-[18px] lg:text-[20px] text-center">
+        <p className="text-black font-bold text-[35px] leading-[40px] text-center 2xl:hidden block">
+          Welcome to Niger <br />
+          Delta Viewpoint.
+        </p>
+        <p className="text-black font-medium py-5 text-[18px] lg:text-[20px] text-center 2xl:block hidden">
           Exposition, Trade Show, Empowerment, Conference & <br /> Networking
           Event.
         </p>
-        <div className="py-10 grid lg:grid-cols-2 gap-10 justify-center">
-          <div>
-            <VideoComponent
+        <p className="text-black font-medium py-5 text-[17px] lg:text-[20px] text-center 2xl:hidden block">
+          Exposition, Trade Show, Empowerment, <br /> Conference & Networking
+          Event.
+        </p>
+        <Link href={"#"} className="font-bold text-black text-[18px]">
+          <p className="text-center">Learn more</p>
+        </Link>
+        <div className="hidden 2xl:block">
+          <div className="py-10 grid lg:grid-cols-2 gap-10 justify-center ">
+            <div>
+              {/* <VideoComponent
               src={
                 "https://www.youtube.com/results?search_query=set+up+typography+reuseable+component+on+next"
               }
               width={900}
               height={700}
-            />
+            /> */}
+              <Image
+                src={"/assets/desktop-frame-1.png"}
+                alt=""
+                width={300}
+                height={300}
+                className="w-full"
+              />
+            </div>
+            <div>
+              {/* <VideoComponent
+              src={
+                "https://www.youtube.com/results?search_query=set+up+typography+reuseable+component+on+next"
+              }
+              width={300}
+              height={500}
+            /> */}
+              <Image
+                src={"/assets/desktop-frame-2.png"}
+                alt=""
+                width={300}
+                height={300}
+                className="w-[75%]"
+              />
+
+              {/* <VideoComponent
+              src={
+                "https://www.youtube.com/results?search_query=set+up+typography+reuseable+component+on+next"
+              }
+              width={300}
+              height={500}
+            /> */}
+              <Image
+                src={"/assets/desktop-frame-2.png"}
+                alt=""
+                width={300}
+                height={300}
+                className="w-[75%] mt-5"
+              />
+            </div>
           </div>
-          <div>
-            <VideoComponent
+        </div>
+
+        <div className="block 2xl:hidden">
+          <div className="py-10 justify-center space-y-5 ">
+            {/* <VideoComponent
               src={
                 "https://www.youtube.com/results?search_query=set+up+typography+reuseable+component+on+next"
               }
+              width={900}
+              height={700}
+            /> */}
+            <Image
+              src={"/assets/mobile-frame-1.png"}
+              alt=""
               width={300}
-              height={500}
+              height={300}
+              className="w-full"
             />
-            <VideoComponent
+            {/* <VideoComponent
               src={
                 "https://www.youtube.com/results?search_query=set+up+typography+reuseable+component+on+next"
               }
               width={300}
               height={500}
+            /> */}
+            <Image
+              src={"/assets/mobile-frame-1.png"}
+              alt=""
+              width={300}
+              height={300}
+              className="w-full"
+            />
+
+            {/* <VideoComponent
+              src={
+                "https://www.youtube.com/results?search_query=set+up+typography+reuseable+component+on+next"
+              }
+              width={300}
+              height={500}
+            /> */}
+            <Image
+              src={"/assets/mobile-frame-1.png"}
+              alt=""
+              width={300}
+              height={300}
+              className="w-full"
             />
           </div>
         </div>
       </div>
       <div className="">
-        <Summit />
+        <div className="hidden lg:block">
+          <Summit />
+        </div>
+        <div className="lg:hidden block">
+          <MobileSummit />
+        </div>
         <Keynote img={""} name={""} title={""} />
         <RegSection />
       </div>
