364f878ecfdd9436a942d5a11ce58c623b0bee78
david waza <idemetoemediong2022@gmail.com>
2024-04-03 19:17:30 +0100

    completely fixed about us page

--- src/app/about/components/AboutOrganizers/AboutOrganizers.tsx
+++ src/app/about/components/AboutOrganizers/AboutOrganizers.tsx
@@ -6,19 +6,22 @@ import { GoArrowUpRight } from "react-icons/go";
 const AboutOrganizers = () => {
   return (
     <div className="bg-white px-10 lg:px-20 py-32">
-      <div className="grid lg:grid-cols-2 gap-10">
-        <VideoComponent
-          src={
-            "https://www.youtube.com/results?search_query=set+up+typography+reuseable+component+on+next"
-          }
-          width={700}
-          height={700}
-        />
+      <div className="grid lg:grid-cols-2 gap-10 ">
+        <div className="lg:order-1 order-2">
+          <VideoComponent
+            src={
+              "https://www.youtube.com/results?search_query=set+up+typography+reuseable+component+on+next"
+            }
+            width={700}
+            height={700}
+          />
+        </div>
+
         <div>
           <h1 className="text-black pb-5 font-bold text-[20px]">
             Kudimata Nigeria Limited
           </h1>
-          <h1 className="text-black pb-5 text-[50px] font-bold">
+          <h1 className="text-black pb-5 text-[30px] lg:text-[50px] font-bold leading-[40px]">
             About Organizer
           </h1>
           <p className="text-black text-[20px]">
